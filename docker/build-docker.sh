ecae935ee79bdd7ef15dcfbcdacbe56a96631d16
cevap <dev@i2pmail.org>
2019-12-06 22:29:43 +0100

    docker: minor update

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
-DOCKER_IMAGE=${DOCKER_IMAGE:-ionpay/iond-develop}
+DOCKER_IMAGE=${DOCKER_IMAGE:-ion/iond-develop}
 DOCKER_TAG=${DOCKER_TAG:-latest}
 
 BUILD_DIR=${BUILD_DIR:-.}
