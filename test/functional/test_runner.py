638c95bf3b66713e417eaf456ec0cf503065180d
Alexander Block <ablock84@gmail.com>
2019-12-06 10:05:58 +0100

    Allow re-signing of IS locks when performing retroactive signing (#3219)
    * Implement re-signing of InstantSend inputs when TXs come in via blocks
    
    * Use GetAdjustedTime instead of GetTimeMillis in CSigSharesManager
    
    This allows use of mocktime in tests.
    
    * Expose verifiedProRegTxHash in getpeerinfo and implement wait_for_mnauth
    
    * Allow to wait for IS and CL to NOT happen
    
    * Bump timeout for wait_for_instantlock
    
    * Implement tests for retroactive signing of IS and CLs
    
    * Add wait_for_tx function to IonTestFramework
    
    * Add -whitelist=127.0.0.1 to node0
    
    * Use node3 for isolated block generation
    
    * Don't test for non-receival of TXs on node4/node5
    
    Signed-off-by: cevap <dev@i2pmail.org>

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -72,6 +72,7 @@ BASE_SCRIPTS= [
     'llmq-chainlocks.py', # NOTE: needs ion_hash to pass
     'llmq-simplepose.py', # NOTE: needs ion_hash to pass
     'llmq-is-cl-conflicts.py', # NOTE: needs ion_hash to pass
+    'llmq-is-retroactive.py', # NOTE: needs ion_hash to pass
     'llmq-dkgerrors.py', # NOTE: needs ion_hash to pass
     'dip4-coinbasemerkleroots.py', # NOTE: needs ion_hash to pass
     # vv Tests less than 60s vv
