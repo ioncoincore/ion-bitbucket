dddb9a8e1d1dd0070a169fd8cb30c897180f8780
cevap <dev@i2pmail.org>
2019-12-07 17:26:07 +0100

    contrib: update build-and-upload.sh script

--- contrib/gitian-additional-scripts/build-and-upload.sh
+++ contrib/gitian-additional-scripts/build-and-upload.sh
@@ -1,44 +1,36 @@
 #!/bin/bash
-
-# setup
-# ./gitian-build.py --setup --build $SIGNER $VERSION SHA256 ion-biaries
-# see gitian build.py for further options
-
-# build from branch/commit
-# defaultuploadserver: if you pass it, you have to pass username/password, best was is to configure it in your .ssh/config, example config:
-# 	host defaultuploadserver
-# 	  HostName 192.168.1.234
-# 	  PreferredAuthentications publickey
-# 	  IdentityFile ~/.ssh/id_ecdsa
-# uploadlogs: if used, all logs will be uploaded too
-# uploadfolder: folder on a server where binaries should be placed
-# hash: create hashes for new files
-#./gitian-build.py --detach-sign --commit --no-commit --build --upload defaultuploadserver --uploadlogs --uploadfolder ion-binaries --hash SHA256 $SIGNER $VERSION
-# build from tag/version
-#./gitian-build.py --detach-sign --no-commit --build --upload defaultuploadserver --uploadlogs --uploadfolder ion-binaries --hash SHA256 $SIGNER $VERSION
-
 cd $HOME
 
-# Signer ID,Name, Email ... (use ID listed in gpg --list-keys)
-export SIGNER="7FDF93BCDE169B04"
-# version (tag)/branch/hash
-export VERSION="develop"
+# mandatory settings
+export SIGNER="7FDF93BCDE169B04"        # Signer ID,Name, Email ... (use ID listed in gpg --list-keys)
+export VERSION="develop"                # version (tag)/branch/hash
 
-CURRENTDIR=${PWD}
+# optional settings
+export UPLOAD="defaultuploadserver"
+export HASH="SHA256"                    # hash new files
+export UPLOADFOLDER="ion-binaries"      # folder on UPLOAD server (will be created if not existing)
 
-# To setup run:
-# ./gitian-build.py --setup ${SIGNER} ${VERSION}
-# sudo reboot
+# To setup run and reboot:
+#   ./gitian-build.py --setup ${SIGNER} ${VERSION}
+#   sudo reboot
 
-# cleanup
-rm -fR ./ion ./gitian.sigs ./ion-detached-sigs ./ion-binaries
+# build from branch/commit
+#   defaultuploadserver: if you pass it, you have to pass username/password, best was is to configure it in your .ssh/config, example config:
+#     	host defaultuploadserver
+#     	  HostName 192.168.1.234
+#     	  PreferredAuthentications publickey
+#     	  IdentityFile ~/.ssh/id_ecdsa
+#   uploadlogs: if used, all logs will be uploaded too
+#   uploadfolder: folder on a server where binaries should be placed
+#   hash: create hashes for new files
+# command examples
+#   build from branch/commit (with --commit switch)
+#       ./gitian-build.py --detach-sign --commit --no-commit --build --upload defaultuploadserver --uploadlogs --uploadfolder ion-binaries --hash SHA256 $SIGNER $VERSION
+#   build from tag/version (without --commit switch)
+#       ./gitian-build.py --detach-sign --no-commit --build --upload defaultuploadserver --uploadlogs --uploadfolder ion-binaries --hash SHA256 $SIGNER $VERSION
 
-# download and update
-# over ssh (use SSH with key added to github without password which lets script auto push sigs and whatever you have preset)
-# git clone git@github.com:ioncoin/ion
-# over https
 if [ ! -d ./ion ]; then
-    git clone git@bitbucket.org:ioncoin/ion.git
+    git clone http://bitbucket.org/ioncoin/ion.git
 else
     cd ion;
     git fetch origin -f;
@@ -70,7 +62,7 @@ else
     # update
     cd ./gitian-builder;
     git pull
-    cd ${CURRENTDIR}
+    cd ..
 fi
 
 ./gitian-build.py --detach-sign --commit --no-commit --build --upload defaultuploadserver --uploadlogs --uploadfolder ion-binaries --hash SHA256 $SIGNER $VERSION
