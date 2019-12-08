c0a3d53ce4435fc7bae54e5cc2fba1cd964ee64c
cevap <dev@i2pmail.org>
2019-12-08 11:43:36 +0100

    depends - fix zeromq for i686

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -3,14 +3,16 @@ $(package)_version=4.3.2
 $(package)_download_path=https://github.com/zeromq/libzmq/releases/download/v$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=ebd7b5c830d6428956b67a0454a7f8cbed1de74b3b01e5c33c5378e22740f763
+$(package)_patches=0001-fix-build-with-older-mingw64.patch
 
 define $(package)_set_vars
-  $(package)_config_opts=--without-docs --disable-shared --without-libsodium --disable-curve --disable-curve-keygen --disable-perf --disable-Werror
+  $(package)_config_opts=--without-documentation --disable-shared --without-libsodium --disable-curve
   $(package)_config_opts_linux=--with-pic
   $(package)_cxxflags=-std=c++11
 endef
 
 define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/0001-fix-build-with-older-mingw64.patch && \
    cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub config
 endef
 
