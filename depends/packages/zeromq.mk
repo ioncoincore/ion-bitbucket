94feadc1aa8dbcdfb534d24aad4404bb12cec112
cevap <dev@i2pmail.org>
2019-12-10 20:46:25 +0100

    depends - zeromq cleanup, delete obsolete patches and disable phthread patch

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -3,7 +3,7 @@ $(package)_version=4.3.2
 $(package)_download_path=https://github.com/zeromq/libzmq/releases/download/v$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=ebd7b5c830d6428956b67a0454a7f8cbed1de74b3b01e5c33c5378e22740f763
-$(package)_patches=0001-fix-build-with-older-mingw64.patch 0002-disable-pthread_setname_np.patch 0003-mingw_thread_name_fix.patch
+$(package)_patches=0001-fix-build-with-older-mingw64.patch 0002-mingw_thread_name_fix.patch
 
 define $(package)_set_vars
   $(package)_config_opts=--without-documentation --disable-shared --without-libsodium --disable-curve
@@ -13,8 +13,7 @@ endef
 
 define $(package)_preprocess_cmds
   patch -p1 < $($(package)_patch_dir)/0001-fix-build-with-older-mingw64.patch && \
-  patch -p1 < $($(package)_patch_dir)/0002-disable-pthread_setname_np.patch && \
-  patch -p1 < $($(package)_patch_dir)/0003-mingw_thread_name_fix.patch && \
+  patch -p1 < $($(package)_patch_dir)/0002-mingw_thread_name_fix.patch && \
   cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub config
 endef
 
