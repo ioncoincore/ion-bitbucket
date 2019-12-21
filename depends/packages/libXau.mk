39b846faeacbf6d0514272e2cf8082aca4bb93f0
cevap <dev@i2pmail.org>
2019-12-21 05:09:45 +0100

    depends - libXau, add var

--- depends/packages/libXau.mk
+++ depends/packages/libXau.mk
@@ -6,8 +6,7 @@ $(package)_sha256_hash=fdd477320aeb5cdd67272838722d6b7d544887dfe7de46e1e7cc0c27c
 $(package)_dependencies=xproto
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-shared --disable-lint-library --without-lint
-  $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
+  $(package)_config_opts=--disable-shared
   $(package)_config_opts_linux=--with-pic
 endef
 
