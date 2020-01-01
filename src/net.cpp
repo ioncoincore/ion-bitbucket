f943925d570b5692154c0d8a7c216dff7bf45a93
Alexander Block <ablock84@gmail.com>
2020-01-01 15:13:14 +0100

    Don't disconnect masternode connections when we have less then the desired amount of outbound nodes (#3255)
    Signed-off-by: cevap <dev@i2pmail.org>

--- src/net.cpp
+++ src/net.cpp
@@ -2925,6 +2925,11 @@ size_t CConnman::GetNodeCount(NumConnections flags)
     return nNum;
 }
 
+size_t CConnman::GetMaxOutboundNodeCount()
+{
+    return nMaxOutbound;
+}
+
 void CConnman::GetNodeStats(std::vector<CNodeStats>& vstats)
 {
     vstats.clear();
