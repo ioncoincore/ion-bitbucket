8c30c38c474b281e817cc8ecbaa42dbb49d14b28
cevap <dev@i2pmail.org>
2020-03-10 07:52:39 +0100

    ICC62 - add two new ipv4 hardcoded seeds, delete obsolete, fix port bug

--- contrib/seeds/makeseeds.sh
+++ contrib/seeds/makeseeds.sh
@@ -20,21 +20,14 @@
 #         and check if every IP address has a port in nodes_main.txt and nodes_test.txt
 #   thats it.
 
-MAINSEEDS="95.217.78.168:12700
-73.166.95.119:12700
-95.216.230.101:12700
-167.86.67.2:12700
-164.68.120.4:12700 
-207.148.84.210:12700
-94.130.163.244:12700 
-66.55.147.60:64081
-45.32.80.95:12700
+MAINSEEDS="37.247.55.128:12700
+163.172.188.223:12700
+37.120.155.10:12700
+88.202.231.139:12700
 72.17.88.56:12700
 149.29.2.18:12700
 109.123.118.38:12700
-155.138.225.26:12700
-37.120.155.10:12700
-88.202.231.139:12700"
+155.138.225.26:12700"
 
 MAINONIONSEEDS="mdbpyigvanhrv6jg.onion:12700
 ndqgxcnvvd2s6pbr.onion:12700
@@ -42,12 +35,14 @@ ndqgxcnvvd2s6pbr.onion:12700
 mdbpyigvanhrv6jg.onion:12700
 g2x6xrzpef6fktez.onion:12700"
 
-TESTNETSEEDS="72.17.88.56:27170
+TESTNETSEEDS="37.247.55.128:27170
+163.172.188.223:27170
+37.120.155.10:27170
+88.202.231.139:27170
+72.17.88.56:27170
 149.29.2.18:27170
 109.123.118.38:27170
-155.138.225.26:27170
-37.120.155.10:27170
-88.202.231.139:27170"
+155.138.225.26:27170"
 
 TESTNETONIONSEEDS="mdbpyigvanhrv6jg.onion:27170
 ndqgxcnvvd2s6pbr.onion:27170
@@ -71,6 +66,9 @@ getmainchainzmainseeds () {
     sed -i 's/\"]}]//g' contrib/seeds/nodes_main.txt
     sed -i 's/\"]}/:12700/g' contrib/seeds/nodes_main.txt
     sed -i 's/\\//g' contrib/seeds/nodes_main.txt
+    # delete last 2 line s
+    sed -i '$d' contrib/seeds/nodes_main.txt
+    sed -i '$d' contrib/seeds/nodes_main.txt
 }
 
 updateseeds () {
@@ -94,5 +92,6 @@ setcustomseeds "./contrib/seeds/nodes_main.txt" "$MAINONIONSEEDS"
 setcustomseeds "./contrib/seeds/nodes_test.txt" "$TESTNETSEEDS"
 setcustomseeds "./contrib/seeds/nodes_test.txt" "$TESTNETONIONSEEDS"
 getmainchainzmainseeds
+updateseeds
 sleep 2
 cleanup
